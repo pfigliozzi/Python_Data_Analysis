f79853d7f1d7c10a3c3ae87615cc543eb20f3f4c
Patrick Figliozzi <pfigliozzi@uchicago.edu>
2013-03-26 13:13:35 -0500

    Finish Diatrack Edge Cutting
    Renamed Diatrack Edge Cutting to py a .py file. Also, added dialog input
    and output to the script and made it work correctly.

--- Diatrack Edge Cutting.py
+++ Diatrack Edge Cutting.py
@@ -1,26 +1,33 @@
-import string
+import string, Tkinter, tkFileDialog
+from decimal import *
 
+root = Tkinter.Tk()
+root.withdraw()
+file_path = tkFileDialog.askopenfilename()
 particle=0
 trajdb={}
-for line in open('Mov_01221301_trajs_0.25FOV.txt','r').readlines():
+file_save=tkFileDialog.asksaveasfilename()
+pxcutoff=input('Pixel cutoff around edges=')
+imgsize=input('Pixel image size (x,y)')
+for line in open(file_path,'r').readlines():
     if line.startswith('f'):
         continue
     else:
         particle+=1
         templine=string.split(line)
-        intline=[float(i) for i in templine]
+        intline=[Decimal(i) for i in templine]
         spt=[(particle,intline[0],(len(intline)-1)/3)]
         for k in range(1,len(intline),3):
             spt.append((intline[k],intline[k+1]))
         trajdb[str(particle)]=spt
         print 'Particle '+str(particle)+' is done!'
- trajdb['range']=[1,particle]
+trajdb['range']=[1,particle]
 innertrajs=[]
 for traj in range(1,trajdb['range'][1]):
     singletraj=trajdb[str(traj)]
     trajswitch=0
     for pos in singletraj[1:]:
-        if pos[0]<5 or pos[1]<5 or pos[0]>475 or pos[1]>265:
+        if pos[0]<pxcutoff or pos[1]<pxcutoff or pos[0]>(imgsize[0]-pxcutoff) or pos[1]>(imgsize[1]-pxcutoff):
             print 'Particle '+str(singletraj[0][0])+' is too close to edge!'
             trajswitch=1            
             break
@@ -31,13 +38,13 @@ for traj in range(1,trajdb['range'][1]):
     else:
         innertrajs.append(singletraj)
         print 'Particle '+str(singletraj[0][0])+' is okay'
-out=open('Mov_01221301_inner_trajs.txt','w')
-out=open('Mov_01221301_inner_trajs.txt','a')
+out=open(file_save,'w')
+out=open(file_save,'a')
 for traj in innertrajs:
-    out.write(repr(traj[0][1]).rjust(6))    
+    out.write(str.rjust(str(traj[0][1]),6))    
     for pos in traj[1:]:
-        line=repr(pos[1]).rjust(13),repr(pos[0]).rjust(13),repr(1.00).rjust(13)
-        out.write(line[0]+line[1]+line[2])
+        #line=repr(pos[0]).rjust(13),repr(pos[1]).rjust(13),repr(1.00).rjust(13)
+        out.write(str.rjust(str(pos[0]),13)+str.rjust(str(pos[1]),13)+str.rjust(str(1.00),13))
     out.write('\n')
     print 'Traj '+str(traj[0][0])+' is done'
 out.close()
