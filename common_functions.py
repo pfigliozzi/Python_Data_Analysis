f48aa0edfffc111eb63add741944d769f0472acd
Patrick Figliozzi <pfigliozzi@uchicago.edu>
2014-04-09 10:59:25 -0500

    Now the y_axis_flip() function is correctly fixed
    Needed to use the copy method on the dataframe to make it how I want.
    Also completed the calc_radius function

--- common_functions.py
+++ common_functions.py
@@ -79,8 +79,9 @@ def y_axis_flip(data_frame, img_height):
 	back to values that match the image. It requires knowledge of
 	the height of the original data.
 	'''
-	data_frame['y pos']=abs(data_frame['y pos']-img_height)
-	return data_frame
+	temp_df=data_frame.copy()
+	temp_df['y pos']=abs(temp_df['y pos']-img_height)
+	return temp_df
 
 def least_sq_fit_circle(data_frame):
 	'''Calculate the best fit circle by least squares for the particle
@@ -119,6 +120,7 @@ def calc_radius(x_pos,y_pos,x_cent,y_cent):
 	'''This function finds the radius in polar coordinates with respect to 
 	the centers x_cent and y_cent. This function always produces a positive
 	radius.'''
+	return np.sqrt((x_pos-x_cent)**2 + (y_pos-y_cent)**2)
 	
 def nn_distance_angle_seperation(data_frame, number_of_bins, x_cent, y_cent):
 	'''Seperates the nearest neighbor distances into angular bins based
