1e96d3e7dd17f9e002de3c2c266e09756b272ae5
Patrick Figliozzi <pfigliozzi@uchicago.edu>
2017-01-24 19:16:50 -0600

    Added new function for finding frames with different number of particles with a rolling mode
    Added two functions to help find frames that have the number of
    particles different than a rolling mode through the data set.

--- passing_event_functions.py
+++ passing_event_functions.py
@@ -12,3 +12,35 @@ from PIL import Image, ImageDraw, ImageColor
 import trackpy
 import trackpy.predict
 import glob
+
+def mode_for_rolling(df):
+	'''Pandas does not support calculating the mode for a rolling object. This
+	function will allow me to calculate the mode of a rolling object.
+	'''
+
+	mode, count = scipy.stats.mode(df)
+	return mode
+
+def find_frames_particle_number_diff_rolling_mode(df, window):
+	'''Returns the frame numbers from a DataFrame where the number of 
+	particles in the frame is different from the mode around that frame.
+
+	:param df: DataFrame object without duplicate values that has frames
+	marked by a 'frame' column
+	:param (int) window: The size of the window to calculate the mode around
+	each frame. A window of 5 would calculate the mode including the two
+	frames before and after the current frame.
+	'''
+	
+	particles_per_frame = df.groupby('frame')
+	rolling_mode = df.groupby('frame').apply(mode_for_rolling)
+
+	# Add values of to the rolling mode to the ends with smaller windows
+	for frame in window/2:
+		rolling_mode.iloc[frame] = rolling_mode.iloc[:window].mode()
+		rolling_mode.iloc[-(frame+1)] = rolling_mode.iloc[-window:].mode()
+
+	different_num_particles = particles_per_frame != rolling_mode
+	frames = different_num_particles[different_num_particles==True].index
+
+	return frames
